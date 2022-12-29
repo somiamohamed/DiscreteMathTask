0d40080380368cd2c01d95d03d272faa4bcdc779
somiamohamed <118680203+somiamohamed@users.noreply.github.com>
2022-12-29 22:17:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("enter the first number");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter the second number");
+int n2 = int.Parse(Console.ReadLine());
+
+int i, j, s;
+
+Console.WriteLine("Perfect numbers from " + n1 + " to " + n2 + " :");
+
+for (i = n1; i <= n2; i++)
+{
+    s = 0;
+    for (j = 1; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            s = s + j;
+        }
+    }
+    if (i == s)
+    {
+        Console.WriteLine(i);
+    }
+}
